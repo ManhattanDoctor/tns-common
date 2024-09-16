6b7ec126305cc170ae668cd7d9e5d154f9e33f26
Renat Gubaev <gubaev.r@n-t.io>
2024-09-16 15:16:10 +0300

    no message

--- hlf/acl/transport/Command.ts
+++ hlf/acl/transport/Command.ts
@@ -1,9 +1,9 @@
 export enum CommandName {
-    GENESIS_GET = `ACL:GenesisGet`,
+    GENESIS_GET = 'ACL:GenesisGet',
 
-    USER_GET = `ACL:UserGet`,
-    USER_ADD = `ACL:UserAdd`,
-    USER_EDIT = `ACL:UserEdit`
+    USER_GET = 'ACL:UserGet',
+    USER_ADD = 'ACL:UserAdd',
+    USER_EDIT = 'ACL:UserEdit'
 }
 
 
